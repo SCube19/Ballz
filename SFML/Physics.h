bfd3ad4aa787586491b2a9d44c6a349f982d336e
SCube19 <kondorcs@wp.pl>
2021-11-29 15:12:47 +0100

    #1-commit_1 | Added semi-constant frame time

--- SFML/Physics.h
+++ SFML/Physics.h
@@ -5,4 +5,7 @@ namespace Ballz
 {
 	//Moves ballz according to Newton's law of gravity
 	void gravity(Ball& first, Ball& second);
+	
+	//Aplies all available physics laws 
+	void applyPhysics(std::vector<Ball>& ballz, RenderWindow& window, const int timeStep);
 }
